255b6696b5b3dc4296b077284099de6db475434b
Jacob Mower <jhmower@gmail.com>
2019-08-28 10:34:03 -0600

    hopefully will work

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Jacob Mower on 8/26/19.
+//  Copyright © 2019 Jacob Mower. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Handle the text field's user input through delegate callbacks.
+        nameTextField.delegate = self
+    }
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
+}
+
