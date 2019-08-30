c4a01d1e8b52d6b9cc1fe389e58f794c468ada6c
Jacob Mower <jhmower@gmail.com>
2019-08-30 10:02:46 -0600

    First real commit on food tracker, with photos

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,14 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Jacob Mower on 8/30/19.
+//  Copyright © 2019 Jacob Mower. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+
+}
